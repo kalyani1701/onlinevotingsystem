941b0de3a05bf9090baf82c6f48f4a2007cf2808
Kalyani Shirsath <kalyanishirsath2003@gmail.com>
2022-12-15 11:35:16 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; 2022 Brought To You By Group 12</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2022 Brought To You By Group 12</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
